5ef21d063782c36afe5e7f4702fa876b1554ce8c
icecreamqii <44384698+icecreamqii@users.noreply.github.com>
2018-10-26 14:36:48 +0800

    first plat
    number 1

--- SampleCode/GettingStarted/HelloWorld/main.lua
+++ SampleCode/GettingStarted/HelloWorld/main.lua
@@ -11,7 +11,3 @@
 
 local msgText = display.newText( "Hello, World!", display.contentCenterX, display.contentHeight*0.5, native.systemFont, 60 )
 msgText:setFillColor( 0.2, 1.0, 0.8 )
-local msgText = display.newText( "Hello, World!", display.contentCenterX, display.contentHeight*0.15, native.systemFont, 60 )
-msgText:setFillColor( 0.2, 0.6, 1.0 )
-local msgText = display.newText( "Hello, World!", display.contentCenterX, display.contentHeight*0.85, native.systemFont, 60 )
-msgText:setFillColor( 1.0, 0.6, 0.8 )
